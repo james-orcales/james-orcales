60def1610f9b616f245edc8a59ed87225d70b905
James Orcales <149360196+james-orcales@users.noreply.github.com>
2026-02-03 12:03:09 +0800

    feat(fish): expand jj/git abbreviations and hook direnv

--- dotfiles/common/.config/fish/config.fish
+++ dotfiles/common/.config/fish/config.fish
@@ -3,32 +3,52 @@ if status is-interactive
         abbr --add n nvim .
 
         #    jj
+        set -q BRANCH; or set -gx BRANCH james
         abbr --add a    jj
+        abbr --add aa   jj abandon
         abbr --add ab   jj bookmark
-        abbr --add abc  jj bookmark create
+        abbr --add abc  jj bookmark create $BRANCH
         abbr --add abl  jj bookmark list
-        abbr --add abs  jj bookmark set
-        abbr --add --set-cursor=!! absp jj bookmark set !! --revision @-
-        abbr --add --set-cursor=!! ac jj commit   -m \"!!\"
-        abbr --add --set-cursor=!! ad jj describe -m \"!!\"
+        abbr --add abs  jj bookmark set $BRANCH
+        abbr --add absp jj bookmark set $BRANCH --revision @-
+        abbr --add abt  jj bookmark track $BRANCH
+        abbr --add ac   --set-cursor=!! jj commit   -m \"!!\"
+        abbr --add ad   --set-cursor=!! jj describe -m \"!!\"
+        abbr --add adf  jj diff
+        abbr --add adfs jj diff --stat --from main
         abbr --add ae   jj edit
-        abbr --add ag   jj git
-        abbr --add agp  jj git push
-        abbr --add agf  jj git fetch --tracked
+        abbr --add ag   jj  git
+        abbr --add agp  jj  git push --bookmark james
+        abbr --add agf  jj  git fetch --tracked
+        abbr --add agfp 'jj git fetch --tracked && jj git push'
         abbr --add ah   jj --help
-        abbr --add al   jj log
+        abbr --add al  'jj log -r \'fork_point(@|main)..(@|main) | fork_point(@|main)\' --no-pager'
+        abbr --add all 'jj log -r \'::main\' --no-pager --limit=10'
         abbr --add an   jj new
         abbr --add asp  jj split
-        abbr --add ash  jj show
-        abbr --add asq  jj squash
+        abbr --add ash  jj show --no-pager
+        abbr --add ashs jj show --no-pager --summary
+        abbr --add ashn jj show --name-only
+        abbr --add as   jj squash
+        abbr --add asi  jj squash --interactive
+        abbr --add --set-cursor=!! asft  jj squash -f !! -t
+        abbr --add --set-cursor=!! asift jj squash --interactive -f !! -t
+        abbr --add --set-cursor=!! asrt  jj squash -r !! -t
         abbr --add ast  jj status
         abbr --add au   jj undo
-        abbr --add ar   jj redo
+        abbr --add arv  jj revert --insert-before @ --revisions 
+        abbr --add ard  jj redo
+        abbr --add ars  jj restore --interactive
+        abbr --add --set-cursor=!! arb jj rebase -s !! -A main
+        abbr --add awl jj workspace list
 
         # Git
         abbr --add gc1 git clone --depth=1 
         abbr --add gc1n git clone --depth=1 --no-single-branch
         abbr --add gcfb git clone --filter=blob:none 
+
+        # GitHub
+        abbr --add ghpr 'gh pr create --title=@coderabbitai --fill-verbose --base main --head james; gh pr view --web james'
 
         abbr --add stdrs 'cd $(rustc --print sysroot)/lib/rustlib/src/rust/library/; and nvim .'
         abbr --add stdgo 'cd $(go env GOROOT)/src/; and nvim .'
@@ -47,4 +67,6 @@ if status is-interactive
 
         bind ctrl-h backward-kill-word
         # bind ctrl-w : # i dont know how to erase builtin binds
+
+        direnv hook fish | source
 end
